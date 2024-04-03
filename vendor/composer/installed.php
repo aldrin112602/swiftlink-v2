3ab1ff262376c343deaa31c205e3ce2a981b96ae
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-03 19:48:33 +0800

    Changes:  M .gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'administrator/pims',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5b1c45c0d2b1ff14c54974b03f0ffd30c3a9f1b4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'administrator/pims' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5b1c45c0d2b1ff14c54974b03f0ffd30c3a9f1b4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
