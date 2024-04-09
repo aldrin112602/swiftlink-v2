448b57acf3f4a71a44d36a7e4434a6ca392c9a4c
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-10 07:09:59 +0800

    Changes:  M admin/add_bill.php

--- admin/add_bill.php
+++ admin/add_bill.php
@@ -2,13 +2,16 @@
 
 require_once '../config.php';
 require_once '../global.php';
+require_once "../send_mail.php";
+
+
 $err_msg = null;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $post = validate_post_data($_POST);
 
     if (explode('-', $post['to_date'])[1] == explode('-', $post['from_date'])[1]) {
-       echo "<script>alert('Invalid date, please try again')</script>";
+        echo "<script>alert('Invalid date, please try again')</script>";
     } else {
         $account_no = $post['selected_customer'];
         $to_date = new DateTime($post['to_date']);
@@ -29,18 +32,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $insertQuery = "INSERT INTO user_package (account_no, invoice, package, coverage, total, period, due_date, process_status, variant, status) VALUES ('$account_no', '$invoice', '$package', '$coverage', $total, '$period', '$dueDate', 'Done', 'true', 'Paid')";
 
-        if ($conn->query($insertQuery)) {
-            echo '<script>
-            $(() => {
-                Swal.fire({
-                    icon: "error",
-                    title: "Invalid Date",
-                    text: "Bill added successfully",
-                });
-            })
-          </script>';
-        } else {
-            $error_msg = "Something went wrong, please try again";
+        $email = getRows("account_no='$account_no'", "accounts")[0]['email'];
+
+        $body = "";
+
+        $body .= "<p style='font-size: 18px'>Dear $email,<br><br>";
+        $body .= "We are pleased to inform you that your recent bill has been successfully processed.<br><br>";
+        $body .= "Below are the details of your bill:<br><br>";
+        $body .= "<strong>Account Number:</strong> $account_no<br>";
+        $body .= "<strong>Package:</strong> $package<br>";
+        $body .= "<strong>Coverage:</strong> $coverage<br>";
+        // $body .= "<strong>Total Amount Due:</strong> $total<br>";
+        $body .= "<strong>Invoice Period:</strong> $period<br>";
+        $body .= "<strong>Due Date:</strong> $dueDate<br><br>";
+        $body .= "If you have any questions or concerns regarding this bill, please feel free to contact us.<br><br>";
+        $body .= "Thank you for choosing Swiftlink!<br>";
+        $body .= "</p>";
+
+
+        if (SendMail($email, $body, "Bill added successfully - Swiftlink")) {
+            if ($conn->query($insertQuery)) {
+                echo '<script>
+                $(() => {
+                    Swal.fire({
+                        icon: "success",
+                        title: "Success",
+                        text: "Bill added successfully",
+                    });
+                })
+              </script>';
+            } else {
+                $error_msg = "Something went wrong, please try again";
+            }
         }
     }
 }
