8bc0332802e0411576a558daf659a724e93e1bcc
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-09 20:58:25 +0800

    Changes:  M admin/approved_payment.php

--- admin/approved_payment.php
+++ admin/approved_payment.php
@@ -1,21 +1,40 @@
 <?php
 require_once "../config.php";
 require_once "../global.php";
+require_once "../send_mail.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $invoice = $_POST["invoice"];
-
-    // check if the data is exist
-    if(isDataExists("payment_confirmation", "*", "invoice='$invoice' AND status='Pending'")) {
+    if (isDataExists("payment_confirmation", "*", "invoice='$invoice' AND status='Pending'")) {
         $sql = "UPDATE payment_confirmation SET status='Approved' WHERE invoice='$invoice'";
-        $conn->query($sql);
-        $sql = "UPDATE user_package SET status='Paid' WHERE invoice='$invoice'";
-        $conn->query($sql);
-        setLog('admin', [
-            'account_no' => $_SESSION['account_no'],
-            'category' => 'Activity',
-            'remark' => 'Approved payment confirmation'
-                                    ]);
+
+        $account_no = getRows("invoice='$invoice'", "user_package")[0]['account_no'];
+        $email = getRows("account_no='$account_no'", "accounts")[0]['email'];
+        $body = "
+        <html>
+        <head>
+        <title>Payment Confirmation</title>
+        </head>
+        <body>
+        <p>Dear $email</p>
+        <p style='font-size: 18px;'>We are pleased to inform you that your payment has been successfully confirmed.</p>
+        <p>Thank you for choosing Swiftlink.</p>
+        
+        <p>Best regards,<br/>Swiftlink</p>
+        </body>
+        </html>
+        ";
+
+        if (SendMail($email, $body, "Payment Confirmation Approved - Swiftlink")) {
+            $conn->query($sql);
+            $sql = "UPDATE user_package SET status='Paid' WHERE invoice='$invoice'";
+            $conn->query($sql);
+            setLog('admin', [
+                'account_no' => $_SESSION['account_no'],
+                'category' => 'Activity',
+                'remark' => 'Approved payment confirmation'
+            ]);
+        }
     }
 
     header("Content-Type: application/json");
@@ -24,4 +43,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         'message' => 'Payment Confirmation approved'
     ]);
 }
-
