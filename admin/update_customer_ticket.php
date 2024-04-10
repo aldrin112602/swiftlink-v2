74d3cde45c469e417063afbcb2be8ee1f4fb2ca9
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-10 08:41:16 +0800

    Changes:  M admin/update_customer_ticket.php

--- admin/update_customer_ticket.php
+++ admin/update_customer_ticket.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
 require_once '../config.php';
 require_once '../global.php';
 require_once '../send_mail.php';
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $email = getRows("account_no='$account_no'", "accounts")[0]['email'] ?? 'caballeroaldrin02@gmail.com';
 
     $response;
-    $body = 'Dear '. $email .',<br><br>
+    $body = '<p style="font-size: 18px">Dear ' . $email . ',<br><br>
 
     We hope this message finds you well. We would like to inform you about an issue that has been identified and apologize for any inconvenience it may have caused. Our team is actively working on resolving the matter, and we anticipate having it fixed within the next 2-5 working days.<br><br>
 
@@ -24,31 +24,49 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     Thank you for your cooperation.<br><br>
 
     Best regards,<br>
-    Swiftlink Support Team';
+    Swiftlink Support Team</p>';
 
     $subject = 'Swiftlink - Important Notice Regarding Ticket Report - ' . $report;
 
-    if($status == 'Process') {
-        if(SendMail($email, $body, $subject)) {
+    if ($status == 'Process') {
+        if (SendMail($email, $body, $subject)) {
             $sql = "UPDATE customer_ticket SET status='$status' WHERE account_no='$account_no'";
-            if($conn->query($sql)) {
+            if ($conn->query($sql)) {
                 $response = ['status' => 'success'];
             } else {
                 $response = ['status' => 'failed'];
             }
         } else {
             $response = ['status' => 'failed'];
-        }  
+        }
     } else {
-        $sql = "UPDATE customer_ticket SET status='$status' WHERE account_no='$account_no'";
-        if($conn->query($sql)) {
-            $response = ['status' => 'success'];
+
+        $body = '<p style="font-size: 18px">Dear ' . $email . ',<br><br>
+        We hope this message finds you well. We would like to inform you about an issue that has been identified and apologize for any inconvenience it may have caused.
+        <br>Your ticket has been closed!<br>
+
+        If you have any further questions or concerns, please feel free to reach out to our support team at swiftlink@gmail.com.<br><br>
+
+        Thank you for your cooperation.<br><br>
+
+        Best regards,<br>
+        Swiftlink Support Team </p>';
+
+        $subject = 'Swiftlink - Important Notice Regarding Ticket Report - ' . $report;
+
+        if (SendMail($email, $body, $subject)) {
+            $sql = "UPDATE customer_ticket SET status='$status' WHERE account_no='$account_no'";
+            if ($conn->query($sql)) {
+                $response = ['status' => 'success'];
+            } else {
+                $response = ['status' => 'failed'];
+            }
         } else {
             $response = ['status' => 'failed'];
         }
     }
 
-    
+
 
     header("Content-Type: application/json");
     echo json_encode($response);
