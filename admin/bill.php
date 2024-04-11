c7a4b420245e31f946d352087f332924e68c0749
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-04-11 09:00:25 +0800

    Changes:  M admin/bill.php

--- admin/bill.php
+++ admin/bill.php
@@ -581,8 +581,37 @@ $email = $row['email'] ?? null;
                                     </div>
                                 </div>
 
+                                <div class="d-flex align-items-center justify-content-start gap-2 py-1">
+                                    <span>Show</span>
+                                    <div>
+                                        <select name="" id="entries" class="">
+                                        </select>
+                                        <script>
+                                            $(() => {
+                                                $('#entries').on('change', function() {
+                                                    let entries = $(this).val();
+                                                    let urlParams = new URLSearchParams(window.location.search);
+                                                    if (urlParams.has('entries')) {
+                                                        urlParams.set('entries', entries);
+                                                    } else {
+                                                        urlParams.append('entries', entries);
+                                                    }
+
+                                                    let newUrl = window.location.pathname + '?' + urlParams.toString();
+
+                                                    window.location = newUrl;
+                                                });
+
+                                                for (let i = 10; i <= 4000; i *= 2) {
+                                                    $('#entries').append(`<option ${i == <?= ($_GET['entries'] ?? 0) ?> ? 'selected' : ''} value="${i}">${i}</option>`)
+                                                }
+                                            })
+                                        </script>
+                                    </div><span>entries</span>
+                                </div>
+
                                 <div class="table-responsive" id="tablePreview">
-                                <div class="d-none _header">
+                                    <div class="d-none _header">
                                         <h3 class="fw-bold">Swiftlink</h3>
                                         <div class="row">
                                             <div class="col">
@@ -648,7 +677,7 @@ $email = $row['email'] ?? null;
 
                                             // Pagination parameters
                                             $totalItems = count($data);
-                                            $itemsPerPage = 10;
+                                            $itemsPerPage = $_GET['entries'] ?? 10;
                                             $totalPages = ceil($totalItems / $itemsPerPage);
                                             $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
                                             $current_page = max(1, min($totalPages, intval($current_page)));
@@ -698,7 +727,17 @@ $email = $row['email'] ?? null;
                                                     })
 
                                                     $('#chooseStatus').on('change', function() {
-                                                        location.href = '?status=' + $(this).val().trim();
+                                                        let status = $(this).val();
+                                                        let urlParams = new URLSearchParams(window.location.search);
+                                                        if (urlParams.has('status')) {
+                                                            urlParams.set('status', status);
+                                                        } else {
+                                                            urlParams.append('status', status);
+                                                        }
+
+                                                        let newUrl = window.location.pathname + '?' + urlParams.toString();
+
+                                                        window.location = newUrl;
                                                     })
 
 
